313d9ba70c5d6055ef159c8a14867d6fb21b32e7
정제우 <wpdn3825@naver.com>
2023-03-08 12:55:03 +0900

    fix : 게시글 클릭

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,33 +9,71 @@ display: flex;
 width:100%;
 padding-left: 20px;
 `
-const List = styled.li`
- padding-left : 20px;
- text-align : left;
- border-bottom: 1px solid grey;
- list-style : none;
+const MainModal = styled.div`
+  margin-top : 20px;
+  padding : 20px;
+  background : #eee;
+  text-align : left;
 `
-
+const GoodButton = styled.button`
+  border : none;
+  background : none;
+  &:hober{
+    background-color : black;
+  }
+`
 function App() {
-  let [title, setTitle]=useState('title')
+  let [title, setTitle]=useState(['국수 맛집','돈까스 맛집','짜장면 맛집'])
+  let [good, setGood]=useState([0,0,0])
+  let [modal, setModal]=useState([false,false,false])
+
+  
 
   return (
     <div className="App">
       <MainNav>
         <h1>myBlog</h1>
       </MainNav>
+      <container>
+        제목<input type={'text'}/><br/>
+        내용<input type={'text'}/><br/>
+        <button>게시</button>
+      </container>
       <main>
-        <List>
-          <h3>{title}</h3>
-          <p>2022.02.02</p>
-        </List>
-        <List>
-          <h3>{title}</h3>
-          <p>2022.02.02</p>
-        </List>
+        {title.map((a,i)=>{
+          return(
+            <div>
+            <h3 onClick={()=>{let copyModal = [...modal]
+                                  copyModal[i] = !copyModal[i]
+                                  setModal(copyModal)}}>{ title[i] }</h3>
+            <GoodButton
+            onClick={()=>{let copyGood = [...good]
+                              copyGood[i]++
+                              setGood(copyGood)}
+            }
+            >
+            👍</GoodButton>
+            <span>{ good[i] }</span>
+            <p>2022.02.02</p>
+            {modal[i] === true ? <Modal title={title} i={i}/> : null}
+            <hr/>
+          </div>
+          )
+        })}
+        
       </main>
     </div>
   );
+}
+
+function Modal( {title , i } ) {
+  return(
+    <MainModal>
+      <h4>{title[i]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </MainModal>
+  )
 }
 
 export default App;
